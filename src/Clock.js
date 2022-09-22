23edf441134a33bbab854eab294329d1c4640818
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-22 22:18:15 +0300

    useEffect cleanup

--- src/Clock.js
+++ src/Clock.js
@@ -3,10 +3,15 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
+  const timerID = useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+     // returning a cleanup function
+     return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
